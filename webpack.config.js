19368ec990519bd3cb653d0059296919dfc46cf5
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-04-18 03:22:57 +0700

    Production Build

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,43 @@ const path = require("path");
 
 module.exports = {
   resolve: {
-    modules: [
-      path.resolve('./lib'),
-      path.resolve('./node_modules')
-    ]
+    modules: [path.resolve("./lib"), path.resolve("./node_modules")],
   },
-  entry: ["babel-polyfill", "./lib/renderers/dom.js"],
+  // entry: ["babel-polyfill", "./lib/renderers/dom.js"],
+  entry: {
+    vendor: [
+      "babel-polyfill",
+      "react",
+      "react-dom",
+      "prop-types",
+      "axios",
+      "lodash.debounce",
+      "lodash.pickby",
+    ],
+    app: ["./lib/renderers/dom.js"],
+  },
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    filename: "[name].js",
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }],
+    rules: [{ test: /\.js$/, exclude: /node_modules/, use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-react', '@babel/preset-env']
+      }
+    } 
+  }],
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  }
 };
